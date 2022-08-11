e43c54a66ba65f3a3b88f1a3369945c758945fc9
Vahap Gözenelioğlu <gozeneliogluvahap@gmail.com>
2022-08-11 18:04:43 +0300

    Add and use Fluent Validation library

---
+++ FluentValidators/AddressValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+
+namespace FluentValidationApp.Web.FluentValidators
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public string NotEmptyMessage { get;} = "{PropertyName} is required";
+
+        public AddressValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.Province).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.PostCode).NotEmpty().WithMessage(NotEmptyMessage).MaximumLength(5).WithMessage(
+                "{PropertyName} must be at most {MaxLength} characters long");
+        }
+        
+    }
+}
---
+++ FluentValidators/CustomerValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+using FluentValidationApp.Web.Models;
+
+namespace FluentValidationApp.Web.FluentValidators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public string NotEmptyMessage { get;} = "{PropertyName} is required";
+        public CustomerValidator()
+        {
+            RuleFor(x=>x.Name).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x=>x.Email).NotEmpty().WithMessage(NotEmptyMessage).EmailAddress().WithMessage("Email is not valid");
+            RuleFor(x=>x.Age).NotEmpty().WithMessage(NotEmptyMessage).InclusiveBetween(18,60).WithMessage("Age is not valid");
+            RuleFor(x=>x.BirthDay).NotEmpty().WithMessage(NotEmptyMessage).Must(x =>
+            {
+                return DateTime.Now.AddYears(-18) >= x;
+            }).WithMessage("You must be at least 18 years old");
+            RuleForEach(x => x.Addresses).SetValidator(new AddressValidator());
+        }
+    }
+}
